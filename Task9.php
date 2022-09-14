83fb5ffb97c37f1ffe9aaf203dc151193e1f2739
Nikita <nedelka999@gmail.com>
2022-09-14 20:39:08 +0300

    try 2

--- Task9.php
+++ Task9.php
@@ -13,9 +13,11 @@ class Task9
             if (!is_int($number)) {
                 throw new \InvalidArgumentException('Exception: the argument must be int');
             }
-            if ($number > 0 && !empty($arr)) {
-                return $this->getArr($arr, $number);
+            if ($number <= 0 || empty($arr)) {
+                throw new \InvalidArgumentException('Exception: the argument is incorrect');
             }
+
+            return $this->getArr($arr, $number);
         } catch (\InvalidArgumentException $e) {
             echo $e->getMessage();
         }
@@ -25,7 +27,7 @@ class Task9
         $count = count($arr) - 2;
         for ($i = 0; $i < $count; $i++) {
             if ($arr[$i] + $arr[$i + 1] + $arr[$i + 2] === $number) {
-                $res[] = "${arr[$i]} +  ${arr[$i + 1]} + ${arr[$i + 2]} = $number";
+                $res[] = "${arr[$i]} + ${arr[$i + 1]} + ${arr[$i + 2]} = $number";
             }
         }
 
@@ -34,4 +36,4 @@ class Task9
 }
 
 $obj = new Task9();
-$obj->main([2, 7, 7, 1, 8, 2, 7, 8, 7], 16);
+$res = $obj->main([2, 7, 7, 1, 2, 2, 12, 2, 2], 16);
