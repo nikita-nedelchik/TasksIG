1946782e6ba3310cce0ec7566da3a5cf662debe2
Nikita <nedelka999@gmail.com>
2022-09-15 15:35:33 +0300

    try4

--- Task2.php
+++ Task2.php
@@ -6,26 +6,34 @@ class Task2
 {
     public function main($needDate)
     {
-        try {
-            if (!is_string($needDate)) {
-                throw new \InvalidArgumentException('Exception: the argument must be string');
-            }
-            if (strtotime($needDate) && strtotime($needDate) >= strtotime(date('d-m-Y'))) {
-                return $this->getDate($needDate);
-            }
-        } catch (\InvalidArgumentException $e) {
-            echo $e->getMessage();
+        if (!is_string($needDate)) {
+            throw new \InvalidArgumentException('Exception: the argument must be string');
         }
+        if (!strtotime($needDate)) {
+            throw new \InvalidArgumentException('Exception: the argument is incorrect');
+        }
+        $needArr = explode('-', $needDate);
+        if (!checkdate($needArr[1], $needArr[0], $needArr[2])) {
+            throw new \InvalidArgumentException('Exception: the argument is incorrect');
+        }
+
+        return $this->getDate($needDate);
     }
     private function getDate(string $date): int
     {
-        $date1 = new \DateTime(date('d-m-Y'));
-        $date2 = new \DateTime($date);
-        $interval = $date1->diff($date2);
+        $date1 = strtotime(date('d-m-Y'));
+        $date2 = strtotime($date);
+        if ($date2 < $date1) {
+            $year1 = getdate($date1)['year'];
+            $year2 = getdate($date2)['year'];
+            $diffYears = ($year1 - $year2) + 1;
+            $date2 = strtotime(date('d-m-Y', strtotime($date . "+ $diffYears years")));
+        }
 
-        return (int)($interval->days);
+        $diff = ($date2 - $date1) / 60 / 60 / 24;
+
+        return $diff;
     }
 }
 
 $task2 = new Task2();
-echo $task2->main('0-0-0');
