e1821848a4f92793fc8691e30b3383aba936d449
Nikita <nedelka999@gmail.com>
2022-09-15 12:00:11 +0300

    try 4

--- Task6.php
+++ Task6.php
@@ -6,18 +6,14 @@ class Task6
 {
     public function main($year, $lastYear, $month, $lastMonth)
     {
-        try {
-            if (!is_int($year) || !is_int($lastYear) || !is_int($month) || !is_int($lastMonth)) {
-                throw new \InvalidArgumentException('Exception: the argument must be int');
-            }
-            if (!checkdate($month, 1, $year) || !checkdate($lastMonth, 1, $lastYear)) {
-                throw new \InvalidArgumentException('Exception: the argument has incorrect date');
-            }
-
-            return $this->checkMondays($year, $lastYear, $month, $lastMonth);
-        } catch (\InvalidArgumentException $e) {
-            echo $e->getMessage();
+        if (!is_int($year) || !is_int($lastYear) || !is_int($month) || !is_int($lastMonth)) {
+            throw new \InvalidArgumentException('Exception: the argument must be int');
         }
+        if (!checkdate($month, 1, $year) || !checkdate($lastMonth, 1, $lastYear)) {
+            throw new \InvalidArgumentException('Exception: the argument has incorrect date');
+        }
+
+        return $this->checkMondays($year, $lastYear, $month, $lastMonth);
     }
     private function checkMondays(int $year, int $lastYear, int $month, int $lastMonth, string $day = 'Monday'): int
     {
