8a34754b7e9e5ffa5ddda476f4f8e3265f914377
Nikita <nedelka999@gmail.com>
2022-09-15 12:03:31 +0300

    try4

--- Task7.php
+++ Task7.php
@@ -6,19 +6,17 @@ class Task7
 {
     public function main($arr, $number)
     {
-        try {
-            if (!is_array($arr)) {
-                throw new \InvalidArgumentException('Exception: the argument must be array');
-            } elseif (!is_int($number)) {
-                throw new \InvalidArgumentException('Exception: the argument must be int');
-            } elseif ($number < 0 || empty($arr) || $number >= count($arr)) {
-                throw new \InvalidArgumentException('Exception: the argument is incorrect');
-            }
-
-            return $this->getArray($arr, $number);
-        } catch (\InvalidArgumentException $e) {
-            echo $e->getMessage();
+        if (!is_array($arr)) {
+            throw new \InvalidArgumentException('Exception: the argument must be array');
         }
+        if (!is_int($number)) {
+            throw new \InvalidArgumentException('Exception: the argument must be int');
+        }
+        if ($number < 0 || empty($arr) || $number >= count($arr)) {
+            throw new \InvalidArgumentException('Exception: the argument is incorrect');
+        }
+
+        return $this->getArray($arr, $number);
     }
     private function getArray(array $arr, int $position): array
     {
@@ -30,4 +28,3 @@ class Task7
 
 $myArr = [2, 3, 3, 4];
 $obj = new Task7();
-$obj->main($myArr, 2);
