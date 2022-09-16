05035c18a81f7a6af31db8a6bce3c7ec09e390a4
Nikita <nedelka999@gmail.com>
2022-09-16 18:15:37 +0300

    hope last

--- Task5.php
+++ Task5.php
@@ -12,37 +12,37 @@ class Task5
         if ($n <= 0) {
             throw new \InvalidArgumentException('invalid argument');
         }
-		$first = '0';
-		$second = '1';
-		$fibNum = '';
-		while (strlen($fibNum) < $n) {
-			$fibNum = '';
-			$isMoreTen = false;
-			$second = strrev($second);
-			$first = strrev($first);
-			for ($i = 0; $i < strlen($second); $i++) {
-				$tempVar = @(int)$first[$i] + @(int)$second[$i];
-				if ($isMoreTen) {
-					$tempVar++;
-				}
-				if ($tempVar >= 10) {
-					$isMoreTen = true;
-					$fibNum .= $tempVar % 10;
-				} else {
-					$isMoreTen = false;
-					$fibNum .= $tempVar;
-				}
-			}
-			if ($isMoreTen) {
-				$fibNum .= 1;
-			}
-			$fibNum = strrev($fibNum);
-			$second = strrev($second);
-			$first = $second;
-			$second = $fibNum;
-		}
+        $first = '0';
+        $second = '1';
+        $fibNum = '';
+        while (strlen($fibNum) < $n) {
+            $fibNum = '';
+            $isMoreTen = false;
+            $second = strrev($second);
+            $first = strrev($first);
+            for ($i = 0; $i < strlen($second); $i++) {
+                $tempVar = @(int)$first[$i] + @(int)$second[$i];
+                if ($isMoreTen) {
+                    $tempVar++;
+                }
+                if ($tempVar >= 10) {
+                    $isMoreTen = true;
+                    $fibNum .= $tempVar % 10;
+                } else {
+                    $isMoreTen = false;
+                    $fibNum .= $tempVar;
+                }
+            }
+            if ($isMoreTen) {
+                $fibNum .= 1;
+            }
+            $fibNum = strrev($fibNum);
+            $second = strrev($second);
+            $first = $second;
+            $second = $fibNum;
+        }
 
-		return $fibNum;
+        return $fibNum;
     }
 }
 
