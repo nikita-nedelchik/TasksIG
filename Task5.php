588b535151ecf483820d92996a8e05ae36f91f62
Nikita <nedelka999@gmail.com>
2022-09-16 12:37:21 +0300

    try1

---
+++ Task5.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace src;
+
+class Task5
+{
+    public function main($n)
+    {
+        if (!is_int($n)) {
+            throw new \InvalidArgumentException('invalid argument');
+        }
+        if ($n < 0) {
+            throw new \InvalidArgumentException('invalid argument');
+        }
+
+        return $this->test($n);
+    }
+
+    private function test($n)
+    {
+        foreach ($this->getFibonacci($n) as $i) {
+            if ($i >= $n) {
+                $res = $i;
+                break;
+            }
+        }
+
+        return $res;
+    }
+    private function getFibonacci($n)
+    {
+        $recursion = function ($n) use (&$recursion) {
+            return $n < 2 ? $n : $recursion($n - 1) + $recursion($n - 2);
+        };
+        for ($i = 0; ; $i++) {
+            yield $recursion($i);
+        }
+    }
+}
+
+$task5 = new Task5();
