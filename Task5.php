327f0708166469289aa7fcb05fdb22429a59063c
Nikita <nedelka999@gmail.com>
2022-09-16 18:15:21 +0300

    hope last

--- Task5.php
+++ Task5.php
@@ -9,22 +9,42 @@ class Task5
         if (!is_int($n)) {
             throw new \InvalidArgumentException('invalid argument');
         }
-        if ($n < 0) {
+        if ($n <= 0) {
             throw new \InvalidArgumentException('invalid argument');
         }
-        $first[0] = 1;
-        $first[1] = 1;
-        $fibNum = '';
-        for ($i = 2; ; $i++) {
-            $first[$i % 2] = $first[0] + $first[1];
-            $fibNum = $first[$i % 2];
-            if (strlen($fibNum) >= $n) {
-                break;
-            }
-        }
+		$first = '0';
+		$second = '1';
+		$fibNum = '';
+		while (strlen($fibNum) < $n) {
+			$fibNum = '';
+			$isMoreTen = false;
+			$second = strrev($second);
+			$first = strrev($first);
+			for ($i = 0; $i < strlen($second); $i++) {
+				$tempVar = @(int)$first[$i] + @(int)$second[$i];
+				if ($isMoreTen) {
+					$tempVar++;
+				}
+				if ($tempVar >= 10) {
+					$isMoreTen = true;
+					$fibNum .= $tempVar % 10;
+				} else {
+					$isMoreTen = false;
+					$fibNum .= $tempVar;
+				}
+			}
+			if ($isMoreTen) {
+				$fibNum .= 1;
+			}
+			$fibNum = strrev($fibNum);
+			$second = strrev($second);
+			$first = $second;
+			$second = $fibNum;
+		}
 
-        return $fibNum;
+		return $fibNum;
     }
 }
 
 $task5 = new Task5();
+echo $task5->main(187);
